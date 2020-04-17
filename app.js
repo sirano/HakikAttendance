9566c57e5f7360a9a4a495553e058c29a1e4f411
ray-yhc <raycho521@yonsei.ac.kr>
2020-04-17 10:05:09 +0000

    make datebase
    데이터베이스 틀 만들기
    이제 브랜치 만들어서 새로 갈아엎ㅇ르 예정

--- app.js
+++ app.js
@@ -4,73 +4,19 @@ var url= require('url'); //http, fs, url 모듈 사용 선언
 var qs = require('querystring');
 var express = require('express');
 var sf = require("sf"); //문자열 서식 관련. 참고 : https://jsdev.kr/t/js-string/747
+var mysql = require('mysql');
+var db=mysql.createConnection({
+    host:'localhost',
+    user:'ray',
+    password:'ph.dyhc0521',
+    database:'Members'
+})
+db.connect();
 
-const date=require('./dateFunctions.js');
-
-
-/*
-입력창
--조 선택
--출석부, 인원
-
-계산계산
-결과창
-상세보기
+const cal=require('./lib/calculate.js');
 
 
-__메인메뉴__
-- 메인화면
-v 조 선택
-    v 오늘날짜 선택하기
 
-__입력창__
-v 출첵,공과창 전환하기
-v 팀별로 기록할수 있도록
-    출력 : url에서 특정 조를 표현->pug에서 처리
-    입력 : 해당 조 전달->app.js에서 해당 조 정보 수정
-v 주차별 기록
-    read,writeFile 경로 달라짐
-    
-    __week__
-    v weekly 디렉토리 목록 뽑기
-=>    - 3개이상 ...처리
-=>    -...누르면? 남은목록들 어떻게 보여주지?
-    
-    __출석__
-    v 출석,공과 입력&저장 매커니즘 만들기
-    v 장결자: weekly 와 members 둘 모두 에 있어야 함
-    v New Member 버튼 
-=>    -리팩토라이징
-    -모듈 정리. 함수 밖으로 빼기
-    __공과__
-    v매커니즘 만들기, 저장
-    
-__결과창__
-v 조별 데이터 모으기 : 주차별,반별 / 출석 지각 장결 성경 요절   
-v 조별 데이터 읽어와서 랭킹 만들기
-
-__기타__
-    __뉴멤버__
-        v 등록후 해당주차에 추가
-    v __특이사항__
-    v __중요사항weekly__
-    
-__디자인__
-__데이터__
--json?? mySQL이 뭔지 알아야하지 않ㅇ르까?
-v 매주 이니셜파일 생성하기!!!!!!!
-    v홈을 실행시켰을때
-    v-해당주차 파일이 있는지 없는지 확인한다
-    v-만약없다면 만든다
-    v-teams를 읽는다 -> initial 데이터 만든다
-    v-members를 읽는다 -> 해당팀에 집어넣는다 ->initial 정보를 넣는다
-
-
-___에러___
-- 2020년 3월 20일. 4주차인데 3주차라고 뜬다. dateFunctions 모듈 다시 살펴보기
-    + Date() 결과물이 GMT로 나옴. KST로 바꾸는방법?
-
-*/
 
 var app = express();
 app.locals.pretty=true;    //전송하는 코드를 '보기 이쁘게' 바뀌줌
@@ -160,8 +106,10 @@ app.get('/',(req,res)=>{
     fs.readFile('data/members/teams.json','utf-8',(err,data)=>{
         fs.readdir('data/weekly', (err, dirs) => {
             let teams=JSON.parse(data);
-            let _countWeek=date.countWeek(Date())
+            
+            let _countWeek=cal.countWeek(Date())
             let weekNo=sf("{year:0000}.{month:00}_{weekNo}",_countWeek); //날짜서식 정리
+            
             console.log(weekNo);
             console.log(dirs.indexOf(weekNo+'.json'));  //-1이면 없음, 나머지는 있음
             if(dirs.indexOf(weekNo+'.json')===-1){ //dirs에 이번주 파일이 없다면?
@@ -385,7 +333,7 @@ app.get('/weekResult', (req,res)=>{
     fs.readFile('data/members/teams.json','utf-8',(err,data)=>{
         let teams=JSON.parse(data);
         
-        let _countWeek=date.countWeek(Date())
+        let _countWeek=cal.countWeek(Date())
         let weekNo=sf("{year:0000}.{month:00}_{weekNo}",_countWeek); //날짜서식 정리
         let weekly_data=fs.readFileSync(`data/weekly/${weekNo}.json`,'utf-8');
         let weekly=JSON.parse(weekly_data);
---
+++ data/example.sql
@@ -0,0 +1,127 @@
+-- 치팅시트 : http://www.incodom.kr/DB_-_%EB%8D%B0%EC%9D%B4%ED%84%B0_%ED%83%80%EC%9E%85/MYSQL
+-- 자료형 : https://devhints.io/mysql
+
+----
+-- Table structure for table `members`
+----
+
+CREATE TABLE `members` (
+    
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `name` varchar(10) NOT NULL,
+    `phoneNumber` varchar(15) ,
+    `home` text ,
+    `birthday` varchar(15),
+    `longAbsentee` tinyint(1) DEFAULT 0,
+    `team_id` int(11) NOT NULL,
+    PRIMARY KEY (`id`)
+    
+);
+
+--
+-- Dumping data for table `members`
+--
+
+INSERT INTO `members` (name,team_id) VALUES ('member1', 1);
+INSERT INTO `members` (name,team_id) VALUES ('member2', 1);
+INSERT INTO `members` (name,team_id) VALUES ('member3', 1);
+INSERT INTO `members` (name,team_id) VALUES ('member4', 1);
+INSERT INTO `members` (name,team_id) VALUES ('member5', 2);
+INSERT INTO `members` (name,team_id) VALUES ('member6', 2);
+INSERT INTO `members` (name,team_id) VALUES ('member7', 2);
+INSERT INTO `members` (name,team_id) VALUES ('member8', 2);
+INSERT INTO `members` (name,team_id) VALUES ('member9', 3);
+INSERT INTO `members` (name,team_id) VALUES ('member10', 3);
+INSERT INTO `members` (name,team_id) VALUES ('member11', 3);
+INSERT INTO `members` (name,team_id) VALUES ('member12', 3);
+INSERT INTO `members` (name,team_id) VALUES ('member13', 4);
+INSERT INTO `members` (name,team_id) VALUES ('member14', 4);
+INSERT INTO `members` (name,team_id) VALUES ('member15', 4);
+INSERT INTO `members` (name,team_id) VALUES ('member16', 4);
+INSERT INTO `members` (name,team_id) VALUES ('member17', 5);
+
+
+----
+-- Table structure for table `teams`
+----
+
+CREATE TABLE `teams` (
+    
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `teamName` varchar(10) NOT NULL,
+    `teacher` varchar(10) NOT NULL,
+    `photo` text,
+    `teamInfo` text,
+    PRIMARY KEY (`id`)
+    
+);
+
+--
+-- Dumping data for table `teams`
+--
+
+INSERT INTO `teams` (teamName,teacher,photo) VALUES ('team1', 'teacher1','/img/1.jpg' );
+INSERT INTO `teams` (teamName,teacher,photo) VALUES ('team2', 'teacher2','/img/2.jpg' );
+INSERT INTO `teams` (teamName,teacher,photo) VALUES ('team3', 'teacher3','/img/3.jpg' );
+INSERT INTO `teams` (teamName,teacher,photo) VALUES ('team4', 'teacher4','/img/4.jpg' );
+
+
+
+----
+-- Table structure for table `SI_link`
+----
+
+CREATE TABLE `SI_link` (
+    `mem_id` int(11) NOT NULL,
+    `SI_id` int(11) NOT NULL,
+    
+);
+
+--
+-- Dumping data for table `SI_link`
+--
+INSERT INTO `SI_link` (mem_id,SI_id) VALUES (1,1 );
+
+----
+-- Table structure for table `specialInfos`
+----
+
+CREATE TABLE `specialInfos` (
+    
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `created` datetime NOT NULL,
+    `title` varchar(30) NOT NULL,
+    `description` text,
+    `author` varchar(30),
+    PRIMARY KEY (`id`)
+    
+);
+
+--
+-- Dumping data for table `specialInfos`
+-- select * from members LEFT JOIN ( SI_link LEFT JOIN specialInfos ON SI_link.SI_id=specialInfos.id) ON members.id=SI_link.mem_id;
+--
+INSERT INTO `specialInfos` (created,title) VALUES (NOW(), 'TEST LOG' );
+
+
+----
+-- Table structure for table `weekly`
+----
+
+CREATE TABLE `weekly` (
+    
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `week` float(7,1) NOT NULL, -- 년도년도월월.1 , ex : 202004.2 
+    `mem_id` int(11) NOT NULL ,
+    `attendance` tinyint(1) DEFAULT 0,
+    `bibleRead` tinyint(3) DEFAULT 0,
+    `bibleMemorise` tinyint(1) DEFAULT 0,
+    PRIMARY KEY (`id`)
+);
+
+--
+-- Dumping data for table `weekly`
+--
+
+INSERT INTO `weekly` (week,mem_id) VALUES (140412.1, 1 );
+
